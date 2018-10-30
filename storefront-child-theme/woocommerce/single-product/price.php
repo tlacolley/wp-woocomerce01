656448b1f10a6a0c7b9586326f095bc9ce6bae53
tlacolley <thomaslacolley@gmail.com>
2018-10-30 14:54:46 +0100

    Override function price WooCommerce

--- storefront-child-theme/woocommerce/single-product/price.php
+++ storefront-child-theme/woocommerce/single-product/price.php
@@ -8,4 +8,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $product;
 
 ?>
-<p class="price"><?php echo $product->get_price_html(); ?></p>
+<?php if ($product->get_price()<20): ?>
+<p class="price"><?php echo $product->get_price();; ?>$</p>
+
+<?php else: ?>
+<p class="price"> Attention <?php echo $product->get_price();  ?>$ !!!!</p>
+
+<?php endif ?>
+<p>
+<?php
+// var_dump($product);
+
+ ?>
+</p>
